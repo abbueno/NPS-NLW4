8655e52195123d83c37bd0667477a3a2cfecbe2e
ALLAN BIGHI BUENO <allanbbueno@bol.com.br>
2021-02-22 19:51:37 -0300

    primeiros_passos
    primeiros_passos

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+
+
+app.get("/", (request, response) => {
+    return response.json({mesage: "Hello World - NLW04"});
+});
+
+app.post("/", (request, response) => {
+    return response.json({mesage: "Os dados foram salvos com sucesso!"});
+});
+
+app.listen(3333, () => console.log("Server is running!"));
