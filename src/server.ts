7682111b1aa965549b9723e46512662edaa6fb1d
ALLAN BIGHI BUENO <allanbbueno@bol.com.br>
2021-02-23 14:43:59 -0300

    aula02
    aula02

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,11 @@
 import 'reflect-metadata';
-import express from 'express';
+import express from "express";
 import './database';
+import { router } from "./router";
 
 const app = express();
 
-
-
-app.get("/", (request, response) => {
-    return response.json({mesage: "Hello World - NLW04"});
-});
-
-app.post("/", (request, response) => {
-    return response.json({mesage: "Os dados foram salvos com sucesso!"});
-});
+app.use(express.json());
+app.use(router);
 
 app.listen(3333, () => console.log("Server is running!"));
